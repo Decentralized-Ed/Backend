d1e5fcd281cd5cb6132a613903f25ea31c3c2428
ranjithjupaka <ranjithjupaka@gmail.com>
2023-10-02 12:34:13 +0530

    minor changes

--- routes/veridoc.js
+++ routes/veridoc.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
+const { uploadDoc } = require('../controllers/verridocController')
 const upload = multer({ dest: 'uploads/' })
 
-router.post('/upload-doc', upload.single('pdf'), )
+router.post('/upload-doc', upload.single('pdf'), uploadDoc)
 
 module.exports = router
